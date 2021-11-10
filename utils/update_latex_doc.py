1b529a64799e6c72957786f2e6e582da54aaa926
trGiang99 <dtr.giang.1299@gmail.com>
2021-11-10 16:16:07 +0700

    Revert "update latex format in the converter"
    This reverts commit 0b95b3fa5eb605146b6dadca7dcc0f8afe26361d.

--- utils/update_latex_doc.py
+++ utils/update_latex_doc.py
@@ -27,13 +27,11 @@ def update_latex_doc(tex_path, data):
 
             line = []
             line.append(
-                f"\\section*{{\\huge \\textcolor{{Red}}{{ {word['word']} }} \\small \\textit{{ {word['definition']} }} }}"
+                f"\\section*{{\\huge \\textcolor{{Red}}{{ {word['word']} }} \\small \\textit{word['definition']} }}"
             )
 
             if word['reference']:
-                line.append(
-                    f"Tham khảo: \\footnote{{ \\url{{{word['reference']}}} }}"
-                )
+                line[0] += f" \\footnote{{ {word['reference']} }}"
 
             if word['desciption']:
                 line.append(
@@ -58,10 +56,10 @@ def update_latex_doc(tex_path, data):
                     f"\\subsection*{{Mẹo nhỏ:}}\n{word['tricks']}"
                 )
 
-            tex_file.write('\n'.join(line))
+            tex_file.writelines(line)
 
 
-if __name__ == "__main__":
+if __name__ is "__main__":
 
     word_collections_path = 'collections'
     ebook_path = 'ebook/chapters'
